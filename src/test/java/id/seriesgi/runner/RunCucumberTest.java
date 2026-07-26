4fe62aa79604154bb5f8aee035a0dfaa46d593fd
Muhammad Rizaldy <51598727+Seriesgi@users.noreply.github.com>
2026-07-26 19:06:02 +0700

    Add src/test/java/id/seriesgi/runner/RunCucumberTest.java

---
+++ src/test/java/id/seriesgi/runner/RunCucumberTest.java
@@ -0,0 +1,14 @@
+package id.seriesgi.runner;
+
+import io.cucumber.junit.platform.engine.Constants;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+@ConfigurationParameter(key = Constants.GLUE_PROPERTY_NAME, value = "id.seriesgi")
+public class RunCucumberTest {
+}
